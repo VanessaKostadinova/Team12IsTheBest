342a4fa2e52282c81c01093131f8baac1acbb866
vanes <vanes@100.71.66.138>
2019-10-08 13:27:33 +0100

    Importing LWJGL and creating window

---
+++ Team 12 Project/src/Engine/Main.java
@@ -0,0 +1,68 @@
+package Engine;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFWVidMode.*;
+
+import org.lwjgl.glfw.GLFWVidMode;
+
+public class Main implements Runnable {
+
+	//window dimensions
+	private static int width = 1500;
+	private static int height = 900;
+	
+	//setting up threads
+	private Thread thread;
+	private boolean running = false;
+	
+	//making thread
+	public void start() {
+		thread = new Thread(this, "Game");
+		thread.start();
+	}
+
+	private void init() {
+		
+	}
+	
+	@Override
+	public void run() {
+		init();
+		while(running) {
+			update();
+			render();
+		}
+	}
+	
+	private void update() {
+
+	}
+	
+	private void render() {
+		
+	}
+	
+	//making window
+	public static void main(String[] args) {
+		if(!glfwInit()) {
+			throw new IllegalStateException("GLFW failed to init");
+		}
+		
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		long window = glfwCreateWindow(width, height, "Plague Doctor", 0, 0);
+		if (window == 0) {
+			throw new IllegalStateException("Failed to create window");
+		}
+		
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	    glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2); 
+	    
+	    glfwShowWindow(window);
+	    
+	    while(!glfwWindowShouldClose(window)) {
+	    	glfwPollEvents();
+	    }
+	    
+	    glfwTerminate();
+	}
+}
