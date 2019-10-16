f97d58b3a5e470d6b237527e55dbcba0181f5ae3
InderPanesar <33585829+InderPanesar@users.noreply.github.com>
2019-10-16 23:01:44 +0100

    The Screen implementation has been added.

---
+++ LibGDX/core/src/com/mygdx/game/InsideHouseScreen.java
@@ -0,0 +1,62 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+public class InsideHouseScreen implements Screen {
+	
+	MainScreen screen;
+	Texture img;
+	
+	public InsideHouseScreen(MainScreen mainScreen) {
+		this.screen = mainScreen;
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		screen.batch.begin();
+		screen.batch.draw(img, 0, 0);
+		screen.batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		img.dispose();		
+	}
+
+}
--- LibGDX/core/src/com/mygdx/game/MainScreen.java
+++ LibGDX/core/src/com/mygdx/game/MainScreen.java
@@ -1,13 +1,30 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
-public class MainScreen extends Game {
+public class MainScreen extends Game  {
 
+    public SpriteBatch batch;
+    public Stage ui;
+	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+		batch = new SpriteBatch();
+		ui = new Stage();
+		setScreen(new InsideHouseScreen(this));
 	}
+		
+	@Override
+	public void render() {
+		super.render();
+	}
+	
+	@Override
+	public void dispose() {
+        batch.dispose();
+	}
+	
 
 }
