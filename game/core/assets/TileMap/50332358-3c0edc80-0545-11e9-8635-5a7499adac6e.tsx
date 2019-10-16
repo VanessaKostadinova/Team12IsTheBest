f062db5bb2734f45a5fda06d54fead9ee4560426
InderPanesar <33585829+InderPanesar@users.noreply.github.com>
2019-10-16 18:47:41 +0100

    Deleted Files, LIBGDX

--- game/core/assets/TileMap/50332358-3c0edc80-0545-11e9-8635-5a7499adac6e.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="50332358-3c0edc80-0545-11e9-8635-5a7499adac6e" tilewidth="32" tileheight="32" tilecount="200" columns="20">
- <image source="34td2.png" width="640" height="321"/>
-</tileset>
