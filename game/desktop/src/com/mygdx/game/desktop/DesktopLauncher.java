0aa5d18d7cbbd5a0e59caa2f3bb7620cec8429d4
Inder Panesar <33585829+InderPanesar@users.noreply.github.com>
2019-10-16 18:31:44 +0100

    LIBGDX: Clean File using Scenes (UI/Box2D) : 16/10/19 18:31

---
+++ game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MainScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1920;
+		config.height = 1080;
+		config.vSyncEnabled = true;
+		config.backgroundFPS = 60;
+		config.fullscreen = false;
+		new LwjglApplication(new MainScreen(), config);
+		
+	}
+}
