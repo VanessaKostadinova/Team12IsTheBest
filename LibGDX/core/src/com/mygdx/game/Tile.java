0e20a7dc3f9e8d2c3fc0211915053a4c28aae039
Vanessa Kostadinova <vanessa_g_k@hotmail.com>
2019-10-21 20:25:35 +0100

    Revert "Changes to level and tile classes."
    This reverts commit ba2a451749898f1acc0dde950cbbfea5a19107ed.

--- LibGDX/core/src/com/mygdx/game/Tile.java
+++ LibGDX/core/src/com/mygdx/game/Tile.java
@@ -1,12 +1,10 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
-
 public abstract class Tile {
 	
 	private int coordX;
 	private int coordY;
 	private boolean traversable;
-	private Sprite sprite;
+	
 	
 }
