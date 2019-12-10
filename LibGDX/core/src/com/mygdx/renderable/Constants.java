b4c742aabb4da56c7503444870b8769583f74bb1
Inder Panesar <33585829+InderPanesar@users.noreply.github.com>
2019-12-10 18:34:13 +0000

    Deletion of Previous Version

--- LibGDX/core/src/com/mygdx/renderable/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.renderable;
-
-public class Constants {
-	public static final short PLAYER = 2;
-	public static final short WALL = 4;
-	public static final short SPRAY = 8;
-
-}
--- LibGDX/core/src/com/mygdx/renderable/Living.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.renderable;
-
-public interface Living {
-		
-	public abstract void changeHealth(float deltaHealth);
-	public abstract float getHealth();
-	public abstract boolean isDead();
-
-}
