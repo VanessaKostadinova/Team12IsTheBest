74754fa6c21096327c87566ba9cc67d56b8f2b2c
Inder Panesar <33585829+InderPanesar@users.noreply.github.com>
2020-02-26 18:02:33 +0000

    Updated Main
    Using Dimension, the desktop application will resize to the right window before running.

--- LibGDX/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ LibGDX/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,11 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.Main;
 
+import java.awt.*;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.height = 1080;
-		config.width = 2560;
+		Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+
+		config.height = (int) dimension.getHeight();
+		config.width = (int) dimension.getWidth();
 		config.title = "Plague Doctor";
 		config.fullscreen = true;
 		config.addIcon("icon.png", FileType.Internal);
