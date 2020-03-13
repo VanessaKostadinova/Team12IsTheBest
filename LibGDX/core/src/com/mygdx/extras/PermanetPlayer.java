bff9f0342f80a13accf8340cfb1999ac19e572d0
Vanessa Kostadinova <48443752+VanessaKostadinova@users.noreply.github.com>
2020-03-13 13:22:53 +0000

    Javadoc

--- LibGDX/core/src/com/mygdx/extras/PermanetPlayer.java
+++ LibGDX/core/src/com/mygdx/extras/PermanetPlayer.java
@@ -14,16 +14,31 @@ import java.util.List;
 public class PermanetPlayer {
     public final int TOTAL_NUMBER_OF_NOTES = 10;
 
+    /** The total number of masks held. */
     private int numberOfMasks;
+    /** The total amount of healing fluid. */
     private float healingFluid;
+    /** The total amount of burning fluid. */
     private float burningFluid;
+    /** The total sanity held. */
     private float sanity;
+    /** The total number of energy. */
     private int energy;
+    /** A list of held notes. */
     private List<Note> notes;
+    /** A list of items the player is holding. */
     private Item[] items;
+    /** */
     private int[] chosenItems;
+    /** Stores an instance of the Permanent Player class as per the singleton pattern. */
     private static PermanetPlayer instance;
 
+    /**
+     * Initialises the Permanent Player class.
+     * @param masks Total masks
+     * @param healingFluid Total healing fluid
+     * @param burningFluid Total burning fluid
+     */
     private PermanetPlayer(int masks, float healingFluid, float burningFluid){
         this.numberOfMasks = masks;
         this.healingFluid = healingFluid;
@@ -44,7 +59,14 @@ public class PermanetPlayer {
         return tempItems;
     }
 
-    public static void createInventoryInstance(int masks, float healingFluid, float burningFluid) throws RuntimeException {
+    /**
+     * Creates a new instance of a permanent player
+     * @param masks Total number of masks held
+     * @param healingFluid Total burning fluid held
+     * @param burningFluid Total healing fluid held
+     * @throws RuntimeException If permanent player already exists
+     */
+    public static void createPermanentPlayerInstance(int masks, float healingFluid, float burningFluid) throws RuntimeException {
         if(instance == null){
             instance = new PermanetPlayer(masks, healingFluid, burningFluid);
         }
@@ -53,6 +75,11 @@ public class PermanetPlayer {
         }
     }
 
+    /**
+     * Returns the instance of permanent player
+     * @return PermanentPlayer
+     * @throws RuntimeException If permanent player does not exist.
+     */
     public static PermanetPlayer getPermanentPlayerInstance() throws RuntimeException {
         if(instance != null){
             return instance;
@@ -63,10 +90,19 @@ public class PermanetPlayer {
         }
     }
 
+    /**
+     * Returns a specific item.
+     * @param index The index of the item required.
+     * @return The item at the given index.
+     */
     public Item getItem(int index){
         return items[index];
     }
 
+    /**
+     * Reduces the number of masks held.
+     * @param remove The number of masks you want to remove.
+     */
     public void reduceNumberOfMasks(int remove) {
         numberOfMasks = numberOfMasks - remove;
     }
