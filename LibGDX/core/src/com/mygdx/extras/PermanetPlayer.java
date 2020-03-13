d7c2f2b87eca0a961d7e9d2a9358af4ecdfdc1d1
Inder Panesar <33585829+InderPanesar@users.noreply.github.com>
2020-03-13 15:57:51 +0000

    General Updates

--- LibGDX/core/src/com/mygdx/extras/PermanetPlayer.java
+++ LibGDX/core/src/com/mygdx/extras/PermanetPlayer.java
@@ -38,7 +38,7 @@ public class PermanetPlayer {
     private Item[] createItems(){
         Item[] tempItems = new Item[4];
         tempItems[0] = new Item("BOOTS", "Faster footwear", 75f, 400);
-        tempItems[1] = new Item("MASKS", "Better plague protection TM", 2000f, 400);
+        tempItems[1] = new Item("MASKS", "Better plague protection TM", 30f, 400);
         tempItems[2] = new Item("HEALING STRENGTH", "Better healing", 0.1f, 400);
         tempItems[3] = new Item("BURNING STRENGTH", "Burn baby burn but better", 0.5f, 400);
         return tempItems;
